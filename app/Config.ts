c668a4913d28e24661da552d95a7b12c79e58882
Julijan Andjelic <julijan.andjelic@gmail.com>
2022-11-18 11:54:22 +0100

    added new items to views, new section http

--- app/Config.ts
+++ app/Config.ts
@@ -3,19 +3,37 @@ const conf = {
         path: '/app/routes'
     },
     assets: {
+        // control access to static assets
         allow: function(uri: string) {
             return uri.indexOf('/assets/') === 0;
         }
     },
     views : {
+        // relative to index.ts
         path: '/app/views',
-        componentsPath: 'components'
+
+        // relative to views.path
+        componentsPath: 'components',
+
+        componentAttribute : 'data-component',
+
+        // whether you want to enable the specially handled URI that allows rendering individual components on the server
+        // by default /component/(componentName)/(primaryKey)
+        componentRenderURIEnable : true,
+
+        // this URI is handled by Application
+        // URI matching the pattern componentRenderURI/(componentName)/(primaryKey) will respond with the rendered component
+        // this can be changed, but the URI arguments componentName and primaryKey must be captured with those names
+        componentRenderURI : '/component'
     },
     session: {
         cookieName: 'session',
         keyLength: 32,
         durationSeconds: 60 * 30,
         garbageCollectIntervalSeconds: 60
+    },
+    http: {
+        linkHeaderRel : 'preload' // used by Document.push, can be preload or preconnect
     }
 }
 
