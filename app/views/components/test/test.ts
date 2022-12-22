f993ecd6d13d2df1a87ea9e9dd8125993b317434
Julijan Andjelic <julijan.andjelic@gmail.com>
2022-12-22 11:31:43 +0100

    untrack, not part of the framework

--- app/views/components/test/test.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentScaffold, LooseObject } from "../../../../system/Types";
-
-export default class Test implements ComponentScaffold {
-    async getData(): Promise<LooseObject> {
-        return {
-            test : 3
-        }
-    }
-}
