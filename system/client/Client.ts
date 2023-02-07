3aae536836391ba9364a6e92b171178ad846e26c
Julijan Andjelic <julijan.andjelic@gmail.com>
2023-02-07 15:24:51 +0100

    refs, componentData, improvements

--- system/client/Client.ts
+++ system/client/Client.ts
@@ -8,12 +8,12 @@ export class App {
     // TODO: use config value - currently cant import Conf.ts as it would have to be exposed to client
     // which is a bad idea as it may contain sensitive data
     componentRenderURI: string = '/component';
-    root: Component;
+    root: ClientComponent;
     initializerContext: InitializerFunctionContext;
     store: DataStore = new DataStore();
 
     constructor() {
-        this.root = new Component(null, 'root', document.body, this.store);
+        this.root = new ClientComponent(null, 'root', document.body, this.store);
 
         // this is provided as an argument to each component's initializer function
         this.initializerContext = {
@@ -28,18 +28,21 @@ export class App {
 
 }
 
-export class Component {
+export class ClientComponent {
 
     name: string;
-    children: Array<Component> = [];
-    parent: Component;
+    children: Array<ClientComponent> = [];
+    parent: ClientComponent;
     domNode: HTMLElement;
     isRoot: boolean;
-    root: Component;
+    root: ClientComponent;
     store: DataStoreView;
     storeGlobal: DataStore;
 
     conditionals: Array<HTMLElement> = [];
+    refs: {
+        [key: string] : HTMLElement
+    } = {};
 
     loaded: boolean;
 
@@ -56,7 +59,7 @@ export class Component {
         [key: string] : string
     } = {};
 
-    constructor(parent: Component|null, name: string, domNode: HTMLElement, store: DataStore) {
+    constructor(parent: ClientComponent|null, name: string, domNode: HTMLElement, store: DataStore) {
         this.name = name;
         this.domNode = domNode;
         if (parent === null) {
@@ -68,30 +71,33 @@ export class Component {
             this.root = parent.root;
             this.parent = parent;
         }
-
+        
+        this.initRefs();
         this.initData();
 
         this.storeGlobal = store;
-        this.store = new DataStoreView(store, this.data.componentId);
+        this.store = new DataStoreView(store, this);
 
-        this.initChildren(this.domNode, this);
         this.initConditionals();
+        this.initChildren(this.domNode, this);
 
         this.loaded = ! this.data.if;
+
+        // update conditionals whenever any data in component's store has changed
+        this.store.onChange('*', function () {
+            this.updateConditionals();
+        });
+
+        // update conditionals as soon as component is initialized
+        if (this.conditionals.length > 0) {
+            this.updateConditionals();
+        }
 
         // @ts-ignore
         if (initializers && initializers[this.name]) {
             // @ts-ignore
             this.init(new Function('const init = ' + initializers[this.name] + '; init.apply(this, [...arguments]);'));
         }
-
-        // update conditionals whenever any data in component's store has changed
-        this.store.onChange('*', () => {
-            this.updateConditionals();
-        });
-
-        // update conditionals as soon as component is initialized
-        this.updateConditionals();
     }
 
     // set initializer callback and execute it
@@ -116,6 +122,9 @@ export class Component {
                 if (key === 'componentData') {
                     value = JSON.parse(value);
                 }
+                if (value === null) {
+                    value = '';
+                }
                 this.data[key] = value;
             }
         }
@@ -126,7 +135,7 @@ export class Component {
     public pathData(): Array<{
         [key: string] : string
     }> {
-        let current: Component = this;
+        let current: ClientComponent = this;
         const data = [];
         do {
             data.push(current.data);
@@ -138,9 +147,18 @@ export class Component {
         return data.reverse();
     }
 
+    // set a data value (data-attr)
+    set(key: string, value: string|number) {
+        const dataKey = 'data-' + key;
+        const val = typeof value === 'number' ? value.toString() : value;
+        this.domNode.setAttribute(dataKey, val);
+        this.dataAttributes[dataKey] = val;
+        this.data[toCamelCase(dataKey.substring(5))] = val;
+    }
+
     // first parent that can be redrawn (has no data-use)
-    private redrawableParent(): Component {
-        let current: Component = this;
+    private redrawableParent(): ClientComponent {
+        let current: ClientComponent = this;
         do {
             if (current.isRoot || ! current.data.use) {
                 break;
@@ -150,7 +168,7 @@ export class Component {
         return current;
     }
 
-    private initChildren(scope?: HTMLElement, parent?: Component): void {
+    private initChildren(scope?: HTMLElement, parent?: ClientComponent): void {
 
         if (scope === undefined) {
             scope = this.domNode;
@@ -163,7 +181,7 @@ export class Component {
                 // which is a bad idea as it may contain sensitive data
                 if ((childNode as HTMLElement).hasAttribute('data-component')) {
                     // found a child component, add to children
-                    this.children.push(new Component(parent || null, (childNode as HTMLElement).getAttribute('data-component') || '', childNode as HTMLElement, this.storeGlobal));
+                    this.children.push(new ClientComponent(parent || null, (childNode as HTMLElement).getAttribute('data-component') || '', childNode as HTMLElement, this.storeGlobal));
                 } else {
                     // not a component, resume from here recursively
                     this.initChildren((childNode as HTMLElement), this);
@@ -190,6 +208,8 @@ export class Component {
         //     }]);
         // }
 
+        console.log('redraw', this.name);
+
         // component can't be redrawn if it uses data of it's parent component (data-use)
         // if the current node uses data, get the first parent that can be redrawn
         if (this.data.uses) {
@@ -198,7 +218,7 @@ export class Component {
             return;
         }
 
-        console.log('redraw', this.name);
+        // console.log('redraw', this.name);
         let net = new Net();
         let html = await net.post('/componentRender', {
             component: this.name,
@@ -211,6 +231,13 @@ export class Component {
         this.children = [];
         this.initChildren(this.domNode, this);
 
+        // re-init conditionals and refs
+        this.refs = {};
+        this.conditionals = [];
+        this.initRefs();
+        this.initConditionals();
+        this.updateConditionals();
+
         // run the initializer
         if (this.initializer) {
             this.initializer.apply(this, [{
@@ -222,6 +249,7 @@ export class Component {
     }
 
     private initConditionals(node?: HTMLElement): void {
+        let isSelf = node === undefined;
         if (node === undefined) {
             node = this.domNode;
         }
@@ -231,22 +259,52 @@ export class Component {
         }
 
         node.childNodes.forEach((child) => {
-            if (child.nodeType === 1) {
+            if (child.nodeType === 1 && (isSelf || ! node?.hasAttribute('data-component'))) {
                 this.initConditionals(child as HTMLElement);
             }
         });
     }
 
+    private initRefs(node?: HTMLElement): void {
+        let isSelf = node === undefined;
+        if (node === undefined) {
+            node = this.domNode;
+        }
+
+        if (node.hasAttribute('ref')) {
+            this.refs[node.getAttribute('ref') || 'undefined'] = node;
+        }
+
+        node.childNodes.forEach((child) => {
+            if (child.nodeType === 1 && (isSelf || ! node?.hasAttribute('data-component'))) {
+                this.initRefs(child as HTMLElement);
+            }
+        });
+    }
+
+    public ref<T>(refName: string): T {
+        return this.refs[refName] as T;
+    }
+
     private updateConditionals() {
         this.conditionals.forEach((node) => {
-            const condition = node.getAttribute('data-if');
+            let condition = node.getAttribute('data-if') as string;
             let show: any = false;
+
+            const negated = condition?.indexOf('!') === 0;
+            if (negated) {
+                const conditionMatch = /^!\s*(.*?)$/.exec(condition);
+                if (conditionMatch) {
+                    condition = conditionMatch[1];
+                }
+            }
+
             if (condition) {
                 if (condition.endsWith('()')) {
                     // method
                     // try calling this.[condition]()
                     // it should return a boolean
-                    const prop = this[condition as keyof Component];
+                    const prop = this[condition as keyof ClientComponent];
                     const propType = typeof prop;
                     if (propType !== 'undefined') {
                         if (propType === 'function') {
@@ -261,6 +319,10 @@ export class Component {
                 } else {
                     // prop from components state
                     show = this.store.get(condition);
+                }
+
+                if (negated) {
+                    show = ! show;
                 }
 
                 if (show == true) {
@@ -296,7 +358,7 @@ export class Component {
 
     // travel up the tree until a parent with given parentName is found
     // if no such parent is found returns null
-    public parentFind(parentName: string): Component|null {
+    public parentFind(parentName: string): ClientComponent|null {
         let parent = this.parent;
         while (true) {
             if (parent.name === parentName) {
@@ -314,7 +376,7 @@ export class Component {
     }
 
     // find another component within this component recursively, returns the first found component with given name
-    public find(componentName: string): null|Component {
+    public find(componentName: string): null|ClientComponent {
         for (let i = 0; i < this.children.length; i++) {
             const child = this.children[i];
             if (child.name == componentName) {
@@ -332,7 +394,7 @@ export class Component {
     }
 
     // find another component within this component recursively, returns all found components with given name
-    public query(componentName: string, results: Array<Component> = []): Array<Component> {
+    public query(componentName: string, results: Array<ClientComponent> = []): Array<ClientComponent> {
         for (let i = 0; i < this.children.length; i++) {
             let child = this.children[i];
             if (child.name == componentName) {
@@ -347,11 +409,11 @@ export class Component {
     }
 
     // append to is a selector within this component's dom
-    public async add(appendTo: string, componentName: string, data?: LooseObject, attributes?: {[key: string] : string}) {
+    public async add(appendTo: string|HTMLElement|Element, componentName: string, data?: LooseObject, attributes?: {[key: string] : string}) {
 
-        console.log('create', componentName);
+        // console.log('create', componentName);
 
-        const container = this.domNode.querySelector(appendTo);
+        const container = typeof appendTo === 'string' ? this.domNode.querySelector(appendTo) : appendTo;
 
         if (container === null) {
             console.warn(`${this.name}.add() - appendTo selector not found within this component`);
@@ -371,7 +433,7 @@ export class Component {
 
         const componentNode = tmpContainer.firstChild as HTMLElement;
 
-        const component = new Component(this, componentName, componentNode, this.storeGlobal);
+        const component = new ClientComponent(this, componentName, componentNode, this.storeGlobal);
         this.children.push(component);
 
         container.appendChild(componentNode);
@@ -383,6 +445,15 @@ export class Component {
     //         return prev.concat(curr)
     //     }, []);
     // }
+
+    componentData(key: string): any {
+        if (! this.data.componentData) {
+            return this.data[key];
+        }
+
+        const data = this.data.componentData as unknown as LooseObject;
+        return data[key];
+    }
 
 }
 
@@ -453,6 +524,22 @@ export class Net {
         return this.request('PUT', url, headers, data);
     }
 
+    public async getJSON<T>(url: string, headers?: IncomingHttpHeaders): Promise<T> {
+        return JSON.parse(await this.get(url, headers));
+    }
+
+    public async postJSON<T>(url: string, data: LooseObject, headers?: IncomingHttpHeaders): Promise<T> {
+        return JSON.parse(await this.post(url, data, headers));
+    }
+
+    public async deleteJSON<T>(url: string, headers?: IncomingHttpHeaders) {
+        return JSON.parse(await this.delete(url, headers));
+    }
+
+    public async putJSON<T>(url: string, data: LooseObject, headers?: IncomingHttpHeaders) {
+        return JSON.parse(await this.put(url, data, headers));
+    }
+
 }
 
 export class DataStore {
@@ -470,7 +557,9 @@ export class DataStore {
     } = {}
 
     // return self to allow chained calls to set
-    set(componentId: string, key: string, val: any): DataStore {
+    set(component: ClientComponent, key: string, val: any): DataStore {
+        const componentId = component.data.componentId;
+
         const oldValue = this.get(componentId, key);
 
         if (! this.data[componentId]) {
@@ -482,7 +571,7 @@ export class DataStore {
         if (this.changeListeners[componentId] && (this.changeListeners[componentId][key] || this.changeListeners[componentId]['*'])) {
             // there are change listeners, call them
             (this.changeListeners[componentId][key] || []).concat(this.changeListeners[componentId]['*'] || []).forEach((cb) => {
-                cb(key, val, oldValue, componentId);
+                cb.apply(component, [key, val, oldValue, componentId]);
             });
         }
 
@@ -517,26 +606,26 @@ export class DataStore {
 export class DataStoreView {
 
     private store: DataStore;
-    private componentId: string;
+    private component: ClientComponent;
 
-    constructor(store: DataStore, componentId: string) {
+    constructor(store: DataStore, component: ClientComponent) {
         this.store = store;
-        this.componentId = componentId;
+        this.component = component;
     }
 
     set(key: string, val: any): DataStoreView {
-        this.store.set(this.componentId, key, val);
+        this.store.set(this.component, key, val);
         return this;
     }
 
     get(key: string): any {
-        return this.store.get(this.componentId, key);
+        return this.store.get(this.component.data.componentId, key);
     }
 
     // add callback to be called when a given key's value is changed
     // if key === '*' then it will be called when any of the key's values is changed
     onChange(key: string|AsteriskAny, callback: StoreChangeCallback): DataStoreView {
-        this.store.onChange(this.componentId, key, callback);
+        this.store.onChange(this.component.data.componentId, key, callback);
         return this;
     }
 }
