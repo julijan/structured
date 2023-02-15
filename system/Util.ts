e9f2db7c3fdef7cb922abc336ee9099a1517bbe8
Julijan Andjelic <julijan.andjelic@gmail.com>
2023-02-15 09:44:59 +0100

    attributeValue(To/From)String, toSnakeCase

--- system/Util.ts
+++ system/Util.ts
@@ -10,6 +10,42 @@ export function toCamelCase(dataKey: string, separator: string = '-'): string {
     return dataKey;
 }
 
+// camelCase to snake_case
+export function toSnakeCase(str: string): string {
+    let start = 0;
+    const parts = [];
+    if (str.length < 2) {
+        return str.toLowerCase();
+    }
+
+    // split in parts at capital letters
+    for (let i = 1; i < str.length; i++) {
+        if (str[i] !== str[i].toLowerCase()) {
+            // a capital letter
+            parts.push(str.substring(start, i).toLowerCase());
+            start = i;
+        }
+    }
+    
+    // didn't do anything useful
+    if (start === 0) {
+        return str.toLowerCase();
+    }
+    
+    // add last part
+    parts.push(str.substring(start).toLowerCase());
+
+    return parts.join('_');
+}
+
+export function capitalize(str: string) {
+    return str.substring(0, 1).toUpperCase() + str.substring(1);
+}
+
+export function isAsync(fn: Function): boolean {
+    return fn.constructor.name === 'AsyncFunction';
+}
+
 export function randomString(len: number): string {
     let generators = [
         // uppercase letters
@@ -46,6 +82,7 @@ export function unique<T>(arr: Array<T>): Array<T> {
 }
 
 export function stripTags(contentWithHTML: string, keepTags: Array<string> = []): string {
+    if (contentWithHTML === undefined) {return ''};
     return contentWithHTML.replaceAll(/<\s*\/?\s*[a-zA-Z]+[^>]*?>/g, (sub, index) => {
         const keep = keepTags.some((kept) => {
             const match = new RegExp(`^<\s*\/?\s*${kept}`);
@@ -56,4 +93,38 @@ export function stripTags(contentWithHTML: string, keepTags: Array<string> = [])
         }
         return sub.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
     });
+}
+
+export function attributeValueToString(key: string, value: any): string {
+    return 'base64:' + btoa(JSON.stringify({key, value}));
+}
+
+export function attributeValueFromString(attributeValue: string): string|{
+    key: string,
+    value: any
+} {
+
+    if (attributeValue.indexOf('base64:') === 0) {
+        try {
+            const decoded = atob(attributeValue.substring(7));
+        
+            if (decoded.indexOf('{') !== 0) {
+                // expected to start with "{", if not return as is
+                return attributeValue;
+            }
+        
+            const valObj = JSON.parse(decoded);
+        
+            if (! ('value' in valObj) || ! ('key' in valObj)) {
+                // unrecognized object
+                return decoded;
+            }
+        
+            return valObj;
+    
+        } catch (e) {
+            return attributeValue;
+        }
+    }
+    return attributeValue;
 }
