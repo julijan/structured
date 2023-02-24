c9a41c788cc0ab30fd3af607b289cb2527ef43e8
Julijan Andjelic <julijan.andjelic@gmail.com>
2023-02-24 18:59:33 +0100

    removed samples, those will be covered by docs

--- index.ts
+++ index.ts
@@ -1,24 +1,3 @@
 import { Application } from './system/server/Application.js';
 
 new Application(9090);
-
-// const app = new Application(9090);
-
-// app.on('beforeRequestHandler', async (ctx) => {
-//     console.log('About to handle request', ctx.request.method, ctx.request.url);
-//     if (! ctx.data.requestStart) {
-//         ctx.data.requestStart = {}
-//     }
-
-//     ctx.data.requestStart[ctx.request.url || ''] = new Date().getTime();
-// });
-
-// app.on('afterRequestHandler', async (ctx) => {
-//     let timeStart = ctx.data.requestStart[ctx.request.url || ''];
-//     let time = new Date().getTime() - timeStart;
-//     console.log(`${ctx.request.method} ${ctx.request.url} handled in ${time}ms`);
-// });
-
-// app.on('serverStarted', async () => {
-//     console.log('server has been started');
-// });
