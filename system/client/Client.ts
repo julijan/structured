519d2888786eb9bac375967e88dee79cfcbeb1e2
Julijan Andjelic <julijan.andjelic@gmail.com>
2025-07-26 01:24:10 +0200

    removed unused code from ClientApplication entry point

--- system/client/Client.ts
+++ system/client/Client.ts
@@ -1,9 +1,2 @@
 import { ClientApplication } from './ClientApplication.js';
-import { Net } from './Net.js';
-
-export class Client {
-    Components : ClientApplication = new ClientApplication();
-    Net : Net = new Net();
-}
-
 new ClientApplication();
