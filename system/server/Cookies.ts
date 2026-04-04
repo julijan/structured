7128fdee7d9550b0f4e2b84acf13b3df610f4f49
Julijan Andjelic <julijan.andjelic@gmail.com>
2026-04-04 13:43:35 +0200

    cleaned up Cookies.set method

--- system/server/Cookies.ts
+++ system/server/Cookies.ts
@@ -21,9 +21,33 @@ export class Cookies {
 
     // set a cookie for given response
     // sets the Set-Cookie header, which will be sent with the output
-    public set(response: ServerResponse, name: string, value: string|number, lifetimeSeconds: number, path: string = '/', sameSite: 'Strict' | 'Lax' | 'None' = 'Strict', domain?: string): void {
+    public set(
+        response: ServerResponse,
+        name: string,
+        value: string|number,
+        lifetimeSeconds: number,
+        path: string = '/',
+        sameSite: 'Strict' | 'Lax' | 'None' = 'Strict',
+        domain: string | null = null,
+        secure: boolean = false
+    ): void {
         const expiresAt = lifetimeSeconds > 0 ? new Date(new Date().getTime() + lifetimeSeconds * 1000).toUTCString() : 0;
-        response.appendHeader('Set-Cookie', `${name}=${value}; Expires=${expiresAt}; Path=${path}; SameSite=${sameSite}${domain ? `; domain=${domain}` : ''}`);
+
+        const parts: Array<string> = [];
+        parts.push(`${name}=${value}`);
+        parts.push(`Expires=${expiresAt}`);
+        parts.push(`Path=${path}`);
+        parts.push(`SameSite=${sameSite}`);
+
+        if (typeof domain === 'string') {
+            parts.push(`domain=${domain}`);
+        }
+
+        if (secure) {
+            parts.push('Secure=true');
+        }
+
+        response.appendHeader('Set-Cookie', parts.join('; '));
     }
 
 }
